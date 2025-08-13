54ca4d8fb97daf7744b335e7d623a2e5bf05f045
timexshmurda <israelsoyinka059@gmail.com>
2025-08-13 00:15:18 +0000

    ready to deploy

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export function ThemeProvider({ children, ...props }: React.PropsWithChildren<{}>) {
+export function ThemeProvider({ children, ...props }: React.PropsWithChildren<object>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
+
